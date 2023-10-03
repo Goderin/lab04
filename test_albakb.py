bfa554773c76d28b2d7cf366c467e606d1ae870d
milksop <lockio75@gmail.com>
2023-10-03 19:15:30 +0500

    Akbashev test

---
+++ test_albakb.py
@@ -0,0 +1,7 @@
+from lib import count_common_elements
+list1 = [2, 9, 3, 4]
+list2 = [5, 4, 7, 2]
+list3 = [8, 2, 4, 7]
+list4 = [1, 2, 3, 4]
+count = count_common_elements(list1, list2, list3)
+print(count)
